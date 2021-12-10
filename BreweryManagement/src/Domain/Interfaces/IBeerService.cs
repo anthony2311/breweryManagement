f5feaf3017e31c13bbb87548643338f2d547b57b
Anthony Lammens <anthony.lammens@outlook.com>
2021-12-10 12:43:37 +0100

    add new lib domain project

---
+++ BreweryManagement/src/Domain/Interfaces/IBeerService.cs
@@ -0,0 +1,10 @@
+﻿using Data.Models;
+using System.Collections.Generic;
+
+namespace Api.Controllers
+{
+    public interface IBeerService
+    {
+        List<Beer> Get();
+    }
+}
---
+++ BreweryManagement/src/Domain/Services/BeerService.cs
@@ -0,0 +1,25 @@
+﻿using Api.Controllers;
+using Data.Models;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.Services
+{
+    public class BeerService : IBeerService
+    {
+        private IBeerRepository _beerRepository;
+
+        public BeerService(IBeerRepository beerRepository)
+        {
+            _beerRepository = beerRepository;
+        }
+
+        public List<Beer> Get()
+        {
+            return _beerRepository.GetAll().ToList();
+        }
+    }
+}
